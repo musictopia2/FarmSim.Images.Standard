0f1cfa066a08c1ebe20ac3d4356b425da117dcaa
Andy Walter <musictopia2@hotmail.com>
2026-01-31 17:35:31 -0700

    Add project files.

---
+++ Assets/PngImageAssets.cs
@@ -0,0 +1,44 @@
+#nullable enable
+namespace FarmSim.Images.Standard.Assets;
+public static class PngImageAssets
+{
+    public const string Achievements = "Achievements";
+    public const string AdvancedSettings = "AdvancedSettings";
+    public const string Barn = "Barn";
+    public const string CashRegister = "CashRegister";
+    public const string Chest = "Chest";
+    public const string Coin = "Coin";
+    public const string ConnectingFarms = "ConnectingFarms";
+    public const string Crop = "Crop";
+    public const string Orders = "Orders";
+    public const string PowerGloveWorkshop = "PowerGloveWorkshop";
+    public const string PowerGloveWorksite = "PowerGloveWorksite";
+    public const string Silo = "Silo";
+    public const string SpeedSeed = "SpeedSeed";
+    public const string TimedBoost = "TimedBoost";
+    public const string Tree = "Tree";
+    public const string UnlimitedSpeedSeed = "UnlimitedSpeedSeed";
+    public const string WorkshopDecision = "WorkshopDecision";
+    public const string WorksiteNoSupplies = "WorksiteNoSupplies";
+    public static void Register()
+    {
+        CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.FileFunctions.FileContentRegistry.RegisterFile("Achievements", "/_content/FarmSim.Images.Standard/Achievements.png");
+        CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.FileFunctions.FileContentRegistry.RegisterFile("AdvancedSettings", "/_content/FarmSim.Images.Standard/AdvancedSettings.png");
+        CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.FileFunctions.FileContentRegistry.RegisterFile("Barn", "/_content/FarmSim.Images.Standard/Barn.png");
+        CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.FileFunctions.FileContentRegistry.RegisterFile("CashRegister", "/_content/FarmSim.Images.Standard/CashRegister.png");
+        CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.FileFunctions.FileContentRegistry.RegisterFile("Chest", "/_content/FarmSim.Images.Standard/Chest.png");
+        CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.FileFunctions.FileContentRegistry.RegisterFile("Coin", "/_content/FarmSim.Images.Standard/Coin.png");
+        CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.FileFunctions.FileContentRegistry.RegisterFile("ConnectingFarms", "/_content/FarmSim.Images.Standard/ConnectingFarms.png");
+        CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.FileFunctions.FileContentRegistry.RegisterFile("Crop", "/_content/FarmSim.Images.Standard/Crop.png");
+        CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.FileFunctions.FileContentRegistry.RegisterFile("Orders", "/_content/FarmSim.Images.Standard/Orders.png");
+        CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.FileFunctions.FileContentRegistry.RegisterFile("PowerGloveWorkshop", "/_content/FarmSim.Images.Standard/PowerGloveWorkshop.png");
+        CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.FileFunctions.FileContentRegistry.RegisterFile("PowerGloveWorksite", "/_content/FarmSim.Images.Standard/PowerGloveWorksite.png");
+        CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.FileFunctions.FileContentRegistry.RegisterFile("Silo", "/_content/FarmSim.Images.Standard/Silo.png");
+        CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.FileFunctions.FileContentRegistry.RegisterFile("SpeedSeed", "/_content/FarmSim.Images.Standard/SpeedSeed.png");
+        CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.FileFunctions.FileContentRegistry.RegisterFile("TimedBoost", "/_content/FarmSim.Images.Standard/TimedBoost.png");
+        CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.FileFunctions.FileContentRegistry.RegisterFile("Tree", "/_content/FarmSim.Images.Standard/Tree.png");
+        CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.FileFunctions.FileContentRegistry.RegisterFile("UnlimitedSpeedSeed", "/_content/FarmSim.Images.Standard/UnlimitedSpeedSeed.png");
+        CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.FileFunctions.FileContentRegistry.RegisterFile("WorkshopDecision", "/_content/FarmSim.Images.Standard/WorkshopDecision.png");
+        CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.FileFunctions.FileContentRegistry.RegisterFile("WorksiteNoSupplies", "/_content/FarmSim.Images.Standard/WorksiteNoSupplies.png");
+    }
+}
